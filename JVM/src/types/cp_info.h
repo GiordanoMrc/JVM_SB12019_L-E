85012655b09d87527e871106a60b693b430cdd8d
Danilo Santos <dandesenvolvimento@gmail.com>
2019-05-18 16:36:12 -0300

    atualizando cp_info

--- JVM/src/types/cp_info.h
+++ JVM/src/types/cp_info.h
@@ -23,63 +23,61 @@ typedef enum tag {
 typedef struct cp_info {
     u1 tag;
     union info {
-        struct CONSTANT_Class_info;
-        struct CONSTANT_Fieldref_info;
+        struct CONSTANT_Class_info {
+            u2 name_index;
+        };
+
+        struct CONSTANT_Fieldref_info {
+            u2 class_index;
+            u2 name_and_type_index;
+        };
+
+        struct CONSTANT_NameAndType_info {
+            u2 name_index;
+            u2 descriptor_index;
+        };
+
+        struct CONSTANT_Utf8_info {
+            u2 length;
+            u1 *bytes;
+        };
+
+        struct CONSTANT_Methodref_info {
+            u2 class_index;
+            u2 name_and_type_index;
+        };
+
+        struct CONSTANT_InterfaceMethodref_info {
+            u2 class_index;
+            u2 name_and_type_index;
+        };
+
+        struct CONSTANT_String_info {
+            u2 string_index;
+        };
+
+        struct CONSTANT_Integer_info {
+            u4 bytes;
+        };
+
+        struct CONSTANT_Integer_info {
+            u4 bytes;
+        };
+
+        struct CONSTANT_Float_info {
+            u4 bytes;
+        };
+
+        struct CONSTANT_Long_info {
+            u4 high_bytes;
+            u4 low_bytes;
+        };
+
+        struct CONSTANT_Double_info {
+            u4 high_bytes;
+            u4 low_bytes;
+        };
     };
 } cp_info;
 
-struct CONSTANT_Class_info {
-    u2 name_index;
-};
-
-struct CONSTANT_Fieldref_info {
-    u2 class_index;
-    u2 name_and_type_index;
-};
-
-struct CONSTANT_NameAndType_info {
-    u2 name_index;
-    u2 descriptor_index;
-};
-
-struct CONSTANT_Utf8_info {
-    u2 length;
-    u1 *bytes;
-};
-
-struct CONSTANT_Methodref_info {
-    u2 class_index;
-    u2 name_and_type_index;
-};
-
-struct CONSTANT_InterfaceMethodref_info {
-    u2 class_index;
-    u2 name_and_type_index;
-};
-
-struct CONSTANT_String_info {
-    u2 string_index;
-};
-
-struct CONSTANT_Integer_info {
-    u4 bytes;
-};
-
-struct CONSTANT_Integer_info {
-    u4 bytes;
-};
-
-struct CONSTANT_Float_info {
-    u4 bytes;
-};
-
-struct CONSTANT_Long_info {
-    u4 high_bytes;
-    u4 low_bytes;
-};
-
-struct CONSTANT_Double_info {
-    u4 high_bytes;
-    u4 low_bytes;
-};
 #endif
