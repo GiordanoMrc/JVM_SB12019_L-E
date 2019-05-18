09c80536fd357a908df44d510c6fd475c1fc1022
Danilo Santos <dandesenvolvimento@gmail.com>
2019-05-18 19:25:13 -0300

    adicionando class ClassFile

--- JVM/src/leitor.h
+++ JVM/src/leitor.h
@@ -28,4 +28,26 @@ typedef struct classFile {
     attribute_info *attributes;
 } classFile;
 
+class ClassFile {
+   public:
+    u4 magic;          // cafebabe
+    u2 minor_version;  // formato da versão M.m
+    u2 major_version;
+    u2 constant_pool_count;  // contador da constant pool
+    cp_info *constant_pool;  // constantpool sendo ponteiro p/ cp_info
+    u2 access_flags;
+    u2 this_class;
+    u2 super_class;
+    u2 interfaces_count;
+    u2 *interfaces;
+    u2 fields_count;
+    field_info *fields;
+    u2 methods_count;
+    method_info *methods;
+    u2 attributes_count;
+    attribute_info *attributes;
+
+    ClassFile() {}
+};
+
 #endif
