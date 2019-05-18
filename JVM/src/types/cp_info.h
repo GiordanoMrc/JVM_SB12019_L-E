81b42c560e74e085bea0cee5c124a4b329d60444
Danilo Santos <dandesenvolvimento@gmail.com>
2019-05-18 16:51:08 -0300

    finalizado header cp_info

--- JVM/src/types/cp_info.h
+++ JVM/src/types/cp_info.h
@@ -25,59 +25,55 @@ typedef struct cp_info {
     union info {
         struct CONSTANT_Class_info {
             u2 name_index;
-        };
+        } CONSTANT_Class_info;
 
         struct CONSTANT_Fieldref_info {
             u2 class_index;
             u2 name_and_type_index;
-        };
+        } CONSTANT_Fieldref_info;
 
         struct CONSTANT_NameAndType_info {
             u2 name_index;
             u2 descriptor_index;
-        };
+        } CONSTANT_NameAndType_info;
 
         struct CONSTANT_Utf8_info {
             u2 length;
             u1 *bytes;
-        };
+        } CONSTANT_Utf8_info;
 
         struct CONSTANT_Methodref_info {
             u2 class_index;
             u2 name_and_type_index;
-        };
+        } CONSTANT_Methodref_info;
 
         struct CONSTANT_InterfaceMethodref_info {
             u2 class_index;
             u2 name_and_type_index;
-        };
+        } CONSTANT_InterfaceMethodref_info;
 
         struct CONSTANT_String_info {
             u2 string_index;
-        };
+        } CONSTANT_String_info;
 
         struct CONSTANT_Integer_info {
             u4 bytes;
-        };
-
-        struct CONSTANT_Integer_info {
-            u4 bytes;
-        };
+        } CONSTANT_Integer_info;
 
         struct CONSTANT_Float_info {
             u4 bytes;
-        };
+        } CONSTANT_Float_info;
 
         struct CONSTANT_Long_info {
             u4 high_bytes;
             u4 low_bytes;
-        };
+        } CONSTANT_Long_info;
 
         struct CONSTANT_Double_info {
             u4 high_bytes;
             u4 low_bytes;
-        };
-    };
+        } CONSTANT_Double_info;
+    } info;
 } cp_info;
 
 #endif
