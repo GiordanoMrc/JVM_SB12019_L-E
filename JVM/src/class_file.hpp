26a2c928346c7d8978634c34d87bbd21b148a206
Danilo Santos <dandesenvolvimento@gmail.com>
2019-05-24 16:15:09 -0300

    .

--- JVM/src/class_file.hpp
+++ JVM/src/class_file.hpp
@@ -1,21 +1,16 @@
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string>
-#include "cp_info.h"
-#include "field_info.h"
-#include "method_info.h"
-
 #ifndef CLASS_FILE_H
 #define CLASS_FILE_H
 
+#include "cp_info.h"
+#include "field_info.h"
+#include "method_info.h"
 
 class ClassFile {
    public:
     u4 magic;          // cafebabe
     u2 minor_version;  // formato da versão M.m
     u2 major_version;
-    u2 cp_count;  // contador da constant pool
+    u2 cp_count;             // contador da constant pool
     cp_info *constant_pool;  // constantpool sendo ponteiro p/ cp_info
     u2 access_flags;
     u2 this_class;
