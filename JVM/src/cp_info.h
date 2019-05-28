fcc378fbd4b67f927d5c54d6b88203197c0512c0
GiordanoMrc <giordano.marcelo@gmail.com>
2019-05-27 21:04:15 -0300

    colocando as estrutoras de metodos e atributos

--- JVM/src/cp_info.h
+++ JVM/src/cp_info.h
@@ -19,6 +19,26 @@ enum {
     CONSTANT_MethodType = 16,
     CONSTANT_InvokeDynamic = 18
 };
+
+typedef struct field_info fieldInfo;
+typedef struct attribute_info attribute_info;
+//attributes
+typedef struct ConstantValue_attribute ConstantValue_attribute;
+typedef struct Exception_table_info Exception_table_info;
+typedef struct Code_attributes Code_attributes;
+typedef struct Exceptions_attribute Exceptions_attribute;
+typedef struct classes_info classes_info;
+typedef struct Syntethic_attribute Syntethic_attribute;
+typedef struct SourceFile_attribute SourceFile_attribute;
+typedef struct LineNumberTable_attributes LineNumberTable_attributes;
+typedef struct lineNumberTable lineNumberTable;
+typedef struct LocalVariableTable_attribute LocalVariableTable_attribute;
+typedef struct local_variable_table local_variable_table;
+typedef struct Deprecated_attribute Deprecated_attribute;
+//methods
+typedef struct MethodInfo MethodInfo;
+
+
 }
 typedef struct CONSTANT_Class_info {
     u2 name_index;
@@ -72,25 +92,73 @@ typedef struct CONSTANT_Double_info {
 } CONSTANT_Double_info;
 
 
+typedef struct CONSTANT_MethodHandle_info {
+    u1 tag;
+    u1 reference_kind;
+    u2 reference_index;
+} CONSTANT_MethodHandle;
+
+typedef struct CONSTANT_MethodType_info
+{
+    u1 tag;
+    u2 descriptor_index;
+} CONSTANT_MethodType;
+
+struct CONSTANT_InvokeDynamic_info
+{
+    u1 tag;
+    u2 bootstrap_method_attr_index;
+    u2 name_and_type_index;
+} CONSTANT_InvokeDynamic_info;
+
+struct SourceFile_attribute{
+    u2 sourcefile_index;
+};
+
+struct lineNumberTable{
+    u2 start_pc;
+    u2 line_number;
+};
+struct LineNumberTable_attributes{
+    u2 line_number_table_length;
+    lineNumberTable* lineTable;
+};
+struct local_variable_table{
+    u2 start_pc;
+    u2 length;
+    u2 name_index;
+    u2 descriptor_index;
+    u2 index;
+};
+
+
+struct LocalVariableTable_attribute{
+    u2 local_variable_table_length;
+    local_variable_table* local_table;
+};
+
+struct Deprecated_attribute{
+    //vazio
+};
 //teste!
 
-typedef struct ConstantValue_attribute{
+struct ConstantValue_attribute{
     u2 attribute_name_index;
     u4 attribute_length;
     u2 constantvalue_index;
 
-}ConstantValue_attribute;
+};
 
-typedef struct Exception_table_info{
+struct Exception_table_info{
     u2 start_pc;
     u2 end_pc;
     u2 handler_pc;
     u2 catch_type;
 
-}Exception_table_info;
+};
 
 
-typedef struct Code_attributes {
+struct Code_attributes {
     u2 attribute_name_index;
     u4 attribute_length;
     u2 max_stack;
@@ -101,41 +169,33 @@ typedef struct Code_attributes {
     Exception_table_info* exception_table;
     u2 attributes_count;
     attribute_info* attributes;
-}Code_attributes;
+};
 
-typedef struct classes_info{
+struct classes_info{
     u2 inner_class_info_index;
     u2 outer_class_info_index;
     u2 inner_name_index;
     u2 inner_class_access_flags;
-}classes_info;
+};
 
-typedef struct InnerClasses_attribute{
+struct InnerClasses_attribute{
     u2 number_of_classes;
     classes_info* classes;
-}InnerClasses_attribute;
+};
 
 
-typedef struct Exceptions_attribute{
+struct Exceptions_attribute{
     u2 number_of_exceptions;
     u2* exceptions_index_table;
-}Exceptions_attribute;
+};
 
 
-typedef struct Syntethic_attribute{
+struct Syntethic_attribute{
     // ela nao possui nenhum atributo principal.
-} Syntethic_attribute;
+};
 
 
-typedef struct field_info {
-    u2 access_flags;
-    u2 name_index;
-    u2 descriptor_index;
-    u2 attributes_count;
-    attribute_info* attributes;
-}field_info;
-
-typedef struct attribute_info{
+struct attribute_info{
     u2 attribute_name_index;
     u4 attribute_length;
     union{
@@ -158,6 +218,14 @@ struct MethodInfo {
     u2 attributes_count;
     attribute_info* attributes;
 
+};
+
+struct field_info {
+    u2 access_flags;
+    u2 name_index;
+    u2 descriptor_index;
+    u2 attributes_count;
+    attribute_info* attributes;
 };
 //acabou teste
 
