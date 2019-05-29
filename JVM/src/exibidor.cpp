c941bd24a1410417bfa1780469b7d5abe5833695
GiordanoMrc <giordano.marcelo@gmail.com>
2019-05-28 22:17:25 -0300

    quase acabando

--- JVM/src/exibidor.cpp
+++ JVM/src/exibidor.cpp
@@ -11,13 +11,13 @@ void Printer::showClassFile(ClassFile cf) {
     print_fields_count(cf);
     print_methods_count(cf);
     print_interfaces_count(cf);
+    print_attributes_count(cf);
 
     print_constant_pool(cf);
     print_interfaces(cf);
     print_fields(cf);
 
 
-    //print_attributes_count(cf);
     // print_methods(cf);
     // print_attributes(cf);
 }
